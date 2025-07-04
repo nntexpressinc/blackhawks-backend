5d80c8bc8f79c74d10738b5d1d36d2b163ea5349
abubakirovxolmirza <abubakirovxolmirza1@gmail.com>
2025-07-04 12:46:59 -0700

    amazon

--- apps/load/admin.py
+++ apps/load/admin.py
@@ -9,6 +9,28 @@ from apps.load.models import (
     DispatcherTags, EmployeeTags, CustomerBroker, 
     Stops, Employee, OtherPay, Commodities)
 
+from django.contrib import admin
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.contrib import messages
+from apps.load.models.amazon import AmazonRelayPayment, AmazonRelayProcessedRecord
+class AmazonRelayPaymentAdmin(admin.ModelAdmin):
+    list_display = ['uploaded_at', 'status', 'total_amount', 'loads_updated', 'processed_at']
+    list_filter = ['status', 'uploaded_at']
+    readonly_fields = ['uploaded_at', 'processed_at', 'total_amount', 'loads_updated', 'error_message']
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # editing an existing object
+            return self.readonly_fields + ['file']
+        return self.readonly_fields
+
+class AmazonRelayProcessedRecordAdmin(admin.ModelAdmin):
+    list_display = ['payment', 'trip_id', 'load_id', 'gross_pay', 'is_matched', 'matched_load']
+    list_filter = ['is_matched', 'payment__status', 'created_at']
+    search_fields = ['trip_id', 'load_id', 'matched_load__reference_id']
+
+admin.site.register(AmazonRelayPayment, AmazonRelayPaymentAdmin)
+admin.site.register(AmazonRelayProcessedRecord, AmazonRelayProcessedRecordAdmin)
 # admin.site.register(AmazonRelayPayment)
 admin.site.register(DriverExpense)
 admin.site.register(Pay)
